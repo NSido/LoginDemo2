84f72f6cd1b4bdb98832c63d56969a46e8c9af6c
Rami <43989050+EvilComrade@users.noreply.github.com>
2019-04-26 13:31:59 +0200

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	app/src/main/AndroidManifest.xml
    #	app/src/main/java/com/example/logindemo/MainActivity.java

--- app/src/main/java/com/example/logindemo/MainActivity.java
+++ app/src/main/java/com/example/logindemo/MainActivity.java
@@ -17,7 +17,15 @@ public class MainActivity extends AppCompatActivity {
         Button yourButton =  findViewById(R.id.button);
 
         yourButton.setOnClickListener(new OnClickListener(){
-            public void onClick(View v){
+            public void onClick (View v){
+                startActivity(new Intent(MainActivity.this, LoginActivity.class));
+            }
+        });
+
+        Button yourButton2 =  findViewById(R.id.button2);
+
+        yourButton2.setOnClickListener(new OnClickListener(){
+            public void onClick (View v){
                 startActivity(new Intent(MainActivity.this, LoginActivity.class));
             }
         });
