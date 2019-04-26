d0d70b030cbcac8934772058523ad2b3ccddc998
Rami <43989050+EvilComrade@users.noreply.github.com>
2019-04-26 13:19:01 +0200

    added a new button and strings

--- app/src/main/java/com/example/logindemo/MainActivity.java
+++ app/src/main/java/com/example/logindemo/MainActivity.java
@@ -1,7 +1,13 @@
 package com.example.logindemo;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +15,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
+
+        Button loginButton = findViewById(R.id.button);
+    loginButton.setOnClickListener(new View.OnClickListener() {
+
+        public void onClick(View v){
+            startActivity(new Intent(MainActivity.this, LoginActivity.class));
+        }
+    });
+
+
+
+
+
+
+}
+
+
 }
